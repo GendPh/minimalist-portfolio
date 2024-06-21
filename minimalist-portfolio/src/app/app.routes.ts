db0043157b659ce50f102b9b9407a73b74013fcb
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-06-21 14:37:34 +0100

    chore: Update home page title in app.routes.ts

--- minimalist-portfolio/src/app/app.routes.ts
+++ minimalist-portfolio/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   {
-    path: '', component: HomeComponent, title: 'Home',
+    path: '', component: HomeComponent, title: 'Gabriel Ferreira - Portfolio',
   },
   {
     path: 'projects', title: 'Projects', component: ProjectHomeComponent,
